952fa908d1b76167d4c6707401856ea39dc0c7af
Cameron <cclarkjh@gmail.com>
2020-10-07 11:34:16 +0100

    feat(select server): add button component and add complete select server UI

---
+++ app/components/button.tsx
@@ -0,0 +1,47 @@
+import React, { Component } from 'react';
+import { Pressable, Text, Dimensions } from 'react-native';
+
+interface ButtonComponentProps {
+  onPress(): void;
+  text: string;
+}
+class ButtonComponent extends Component<ButtonComponentProps> {
+  constructor(props: ButtonComponentProps) {
+    super(props);
+  }
+
+  render(): JSX.Element {
+    const deviceWidth = Dimensions.get('window').width;
+    const onPress = this.props.onPress;
+    const text = this.props.text;
+    return (
+      <Pressable
+        onPress={onPress}
+        style={[
+          {
+            backgroundColor: '#007AFF',
+            borderRadius: 10,
+            marginHorizontal: 10,
+            maxWidth: 300,
+            width: '100%'
+          },
+          deviceWidth < 500 ? { maxWidth: 500, marginHorizontal: 0 } : {}
+        ]}
+      >
+        <Text
+          style={{
+            textAlign: 'center',
+            textAlignVertical: 'center',
+            fontSize: 18,
+            color: 'white',
+            margin: 16
+          }}
+        >
+          {text}
+        </Text>
+      </Pressable>
+    );
+  }
+}
+
+export default ButtonComponent;
