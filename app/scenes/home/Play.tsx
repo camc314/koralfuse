297c02ea3d779765a0acd1a2b4e3f1dc6f370de0
Cameron <cclarkjh@gmail.com>
2020-10-26 14:22:05 +0000

    feat(playback): add playback reporting

--- app/scenes/home/Play.tsx
+++ app/scenes/home/Play.tsx
@@ -1,15 +1,19 @@
 import React, { createRef, useEffect, useState } from 'react';
 import { View } from 'react-native';
-import { Video } from 'expo-av';
+import { AVPlaybackStatus, Video } from 'expo-av';
 import { api } from '../../services/api';
 import { IOSPlaybackProfile } from '../../services/playback-profiles/playbackProfile';
 import { StackScreenProps } from '@react-navigation/stack';
 import { RootStackParamList } from '../../routes/home';
+import { BaseItemDto, PlaybackInfoResponse } from '../../services/fetch-api';
 
 type Props = StackScreenProps<RootStackParamList, 'Play'>;
 
 export default function VideoPlayer({ route }: Props): JSX.Element {
   const [streamUrl, setStreamUrl] = useState('');
+  const [itemInfo, setItemInfo] = useState({} as BaseItemDto);
+  const [playbackInfo, setPlaybackInfo] = useState({} as PlaybackInfoResponse);
+  const [playbackStatus, setPlaybackStatus] = useState({} as AVPlaybackStatus);
   const player = createRef<Video>();
 
   const getVideoUrl = () => {
@@ -20,6 +24,7 @@ export default function VideoPlayer({ route }: Props): JSX.Element {
       itemId: route.params.itemId,
       deviceProfileDto: IOSPlaybackProfile
     }).then((res) => {
+      setPlaybackInfo(res);
       // If direct Play is supported
       if (
         res?.mediaSources &&
@@ -54,12 +59,78 @@ export default function VideoPlayer({ route }: Props): JSX.Element {
     });
   };
 
+  const getItemInfo = () => {
+    api.ItemsApi.getItems({
+      uId: api.userInfo.user?.id || '',
+      userId: api.userInfo.user?.id,
+      ids: route.params.itemId
+    }).then((result) => {
+      if (result.items) {
+        setItemInfo(result.items[0]);
+      }
+    });
+  };
+
   useEffect(() => {
+    getItemInfo();
     getVideoUrl();
+    return function () {
+      if (playbackStatus.isLoaded) {
+        api.PlaystateApi.onPlaybackStopped({
+          userId: api.userInfo.user?.id || '',
+          itemId: route.params.itemId,
+          playSessionId: playbackInfo.playSessionId,
+          positionTicks: playbackStatus?.positionMillis * 10000
+        });
+      }
+    };
   }, []);
 
   const goFullScreen = () => {
     player.current?.presentFullscreenPlayer();
+  };
+
+  const reportBeginPlayback = (playbackReport: AVPlaybackStatus) => {
+    if (playbackReport.isLoaded) {
+      if (itemInfo.userData?.playbackPositionTicks) {
+        console.log('Resuming!');
+        player.current?.playFromPositionAsync(
+          itemInfo.userData?.playbackPositionTicks / 10000
+        );
+      }
+      api.PlaystateApi.reportPlaybackStart({
+        playbackStartInfo: {
+          itemId: route.params.itemId,
+          playSessionId: playbackInfo.playSessionId,
+          playbackStartTimeTicks:
+            (itemInfo?.userData?.playbackPositionTicks || 0) / 10000
+        }
+      });
+    }
+  };
+
+  const reportPlaybackStatus = (playbackReport: AVPlaybackStatus) => {
+    // TODO Don't report playback when there is only a time update
+    if (playbackReport.isLoaded) {
+      setPlaybackStatus(playbackReport);
+      if (playbackReport.isPlaying === false) {
+        api.PlaystateApi.reportPlaybackProgress({
+          playbackProgressInfo: {
+            itemId: route.params.itemId,
+            isPaused: true,
+            positionTicks: playbackReport.positionMillis * 10000,
+            playSessionId: playbackInfo.playSessionId
+          }
+        });
+      } else {
+        api.PlaystateApi.reportPlaybackProgress({
+          playbackProgressInfo: {
+            itemId: route.params.itemId,
+            positionTicks: playbackReport.positionMillis * 10000
+          }
+        });
+      }
+    }
   };
 
   return (
@@ -77,6 +148,8 @@ export default function VideoPlayer({ route }: Props): JSX.Element {
         isLooping
         onReadyForDisplay={goFullScreen}
         style={{ width: '100%', height: '100%' }}
+        onPlaybackStatusUpdate={reportPlaybackStatus}
+        onLoad={reportBeginPlayback}
       />
     </View>
   );
