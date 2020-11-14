f5e2eb8fc89139b6044e897a818749f944b77889
cameron clark <cameron.clark@hey.com>
2020-11-14 10:43:06 +0000

    feat(loading indicator): add message after 10s telling the user of poor network connections

--- app/components/LoadingComponent.tsx
+++ app/components/LoadingComponent.tsx
@@ -1,7 +1,48 @@
-import React from 'react';
-import { View, ActivityIndicator } from 'react-native';
+import { useTheme } from '@react-navigation/native';
+import React, { useState } from 'react';
+import { useTranslation } from 'react-i18next';
+import { View, ActivityIndicator, Text } from 'react-native';
 
 export function LoadingComponent(): JSX.Element {
+  const [hasTimedOut, sethasTimedOut] = useState(false);
+  setTimeout(function () {
+    sethasTimedOut(true);
+  }, 10000);
+  const { t } = useTranslation();
+  const { colors } = useTheme();
+
+  if (hasTimedOut) {
+    return (
+      <View
+        style={{
+          flex: 1,
+          justifyContent: 'center',
+          alignItems: 'center',
+          padding: 20
+        }}
+      >
+        <Text
+          style={{
+            color: colors.text,
+            textAlign: 'center',
+            fontSize: 25,
+            marginBottom: 20
+          }}
+        >
+          {t('loadingTimeout')}
+        </Text>
+        <Text
+          style={{
+            color: colors.text,
+            textAlign: 'center'
+          }}
+        >
+          {t('onlineAndConnected')}
+        </Text>
+      </View>
+    );
+  }
+
   return (
     <View
       style={{
